c628e3e230b8948c92216bf346050d1e126db9ca
Tritolium <paracrafta@gmail.com>
2020-03-31 23:29:10 +0200

    Exponentialfunktionen allgemein positiv

--- src/coronatracker/CoronaTracker.java
+++ src/coronatracker/CoronaTracker.java
@@ -1,44 +1,34 @@
 package coronatracker;
 
-import leastsquareapproximation.LeastSquareApproximation;
-import matrix.Matrix;
+import leastsquareapproximation.ExponentialApproximation;
 
 public class CoronaTracker {
 	
-	private Matrix cases;
-	private Matrix ln_cases;
+	private int[][] cases;
 	
 	public CoronaTracker(int[][] cases) {
-		this.cases = new Matrix(cases);
+		this.cases = cases;
 	}
 	
 	public int calcNextWeek() {
-		//calculate logarithms
-		ln_cases = new Matrix(cases.getData());
-		for(int i = 0; i < cases.getRows(); i++) {
-			ln_cases.set(i, 1, Math.log(cases.get(i, 1)));
-		}
-		//calculate linear regression line
-		LeastSquareApproximation approx = new LeastSquareApproximation(ln_cases.getRows(), 1);
-		approx.setValues(ln_cases.getData());
-		approx.setup();
 		
-		Matrix regressionline = approx.calc();
-		//calculate exponential function
-		double factor = Math.exp(regressionline.get(1, 0));
-		double exponent = (regressionline.get(0, 0));
+		ExponentialApproximation exponentialFunction = new ExponentialApproximation(cases);		
+		double[] exponential = exponentialFunction.calc();
+		
 		//find next data
-		double max = -1;
-		for(int i = 0; i < cases.getRows(); i++) {
+		int max = -1;
+		for(int i = 0; i < cases.length; i++) {
 			if(max == -1) {
-				max = cases.get(i, 0);
+				max = cases[i][0];
 			} else {
-				if(cases.get(i, 0) > max) {
-					max = cases.get(i, 0);
+				if(cases[i][0] > max) {
+					max = cases[i][0];
 				}
 			}
 		}
 		//prediction for next set of data
+		double factor = exponential[0];
+		double exponent = exponential[1];
 		double prediction = factor * Math.exp(exponent * (max + 1));
 		return (int) Math.round(prediction);
 	}
---
+++ src/leastsquareapproximation/ExponentialApproximation.java
@@ -0,0 +1,42 @@
+package leastsquareapproximation;
+
+import matrix.Matrix;
+
+public class ExponentialApproximation {
+	
+	private Matrix data;
+	
+	public ExponentialApproximation(double[][] data) {
+		this.data = new Matrix(data);
+	}
+	
+	public ExponentialApproximation(int[][] data) {
+		this.data = new Matrix(data);
+	}
+	
+	public double[] calc() {
+		
+		double[] exp = new double[2];
+		
+		//calculate logarithms
+		Matrix ln_data;
+		ln_data = new Matrix(data.getData());
+		for(int i = 0; i < data.getRows(); i++) {
+			ln_data.set(i, 1, Math.log(data.get(i, 1)));
+		}
+		
+		//calculate linear regression line
+		LeastSquareApproximation approx = new LeastSquareApproximation(ln_data.getRows(), 1);
+		approx.setValues(ln_data.getData());
+		approx.setup();
+		
+		Matrix regressionline = approx.calc();
+		
+		//calculate exponential function
+		exp[0] = Math.exp(regressionline.get(1, 0));
+		exp[1] = regressionline.get(0, 0);
+		
+		return exp;				
+	}
+	
+}
